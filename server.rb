c5f329ef408454b4529ff0b0387ae48f06ec2628
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-28 16:02:19 -0400

    Requirement: Visiting /actors will show a list of actors, sorted alphabetically by name. Each actor name is a link to the details page for that actor.   *Compleate

--- server.rb
+++ server.rb
@@ -14,26 +14,49 @@ def db_connection
 end
 
 get '/actors' do
+  query = 'SELECT actors.name, actors.id FROM actors ORDER BY actors.name;'
   @actors = db_connection do |conn|
-    conn.exec('SELECT actors.name, actors.id FROM actors ORDER BY actors.name;')
+    conn.exec(query)
   end
   erb :'actors/index'
 end
 
 get '/actors/:id' do
   id = params[:id]
-  query = "SELECT movie.name, cast_members.character, actors.name, actors.id
-      JOIN cast_members ON cast_members.movie_id = movies.id
-      JOIN actors ON actors.id = cast_members.actor_id WHERE actors.id = #{id}
-      ORDER BY movies.name;"
+  query = "SELECT movies.title, cast_members.character, actors.name, actors.id
+    FROM movies
+    JOIN cast_members
+    ON cast_members.movie_id = movies.id
+    JOIN actors
+    ON cast_members.actor_id = actors.id
+    WHERE actors.id = #{id}
+    ORDER BY movies.title;"
   @actor_movies = db_connection do |conn|
     conn.exec(query)
   end
 erb :'actors/show'
 end
 
-# get '/movies' do
-# end
+get '/movies/:id' do
+  id = params[:id]
+  query = "SELECT movies.id, movies.title, movies.year, movies.rating, genres.name, studios.name
+  FROM movies
+  JOIN genres
+  ON movies.genre_id = genres.id
+  JOIN studios
+  ON movies.studio_id = studios.id
+  WHERE movies.id = #{id}
+  ORDER BY
+  movies.title;"
+  @movies = db_connection do |conn|
+    conn.exec(query)
+  end
+  erb :'movies/index'
+end
+# Visiting /movies will show a table of movies, sorted alphabetically by title.
+# The table includes the movie title, the year it was released, the rating, the genre,
+#  and the studio that produced it. Each movie title is a link to the details page for that movie.
+
 
 # get '/movies/:id' do
 # end
