818eadadf0c1f03d09d6873d4b0c67bc7f0014d5
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-29 12:10:58 -0400

    Added /movies/movie[id] route.

--- server.rb
+++ server.rb
@@ -5,20 +5,36 @@ require 'pry'
 def db_connection
   begin
     connection = PG.connect(dbname: 'movies')
-
     yield(connection)
-
   ensure
     connection.close
   end
 end
 
 get '/actors' do
-  query = 'SELECT actors.name, actors.id FROM actors ORDER BY actors.name;'
+  query = 'SELECT actors.name, actors.id
+  FROM actors
+  ORDER BY actors.name
+  LIMIT 100;'
   @actors = db_connection do |conn|
     conn.exec(query)
   end
   erb :'actors/index'
+end
+
+get '/movies' do
+  query = "SELECT movies.id, movies.title, movies.year, movies.rating, genres.name as genre, studios.name
+    FROM movies
+    JOIN genres
+    ON movies.genre_id = genres.id
+    JOIN studios
+    ON movies.studio_id = studios.id
+    ORDER BY movies.title
+    LIMIT 20;"
+  @movies = db_connection do |conn|
+    conn.exec(query)
+  end
+  erb :'movies/index'
 end
 
 get '/actors/:id' do
@@ -29,33 +45,41 @@ get '/actors/:id' do
     ON cast_members.movie_id = movies.id
     JOIN actors
     ON cast_members.actor_id = actors.id
-    WHERE actors.id = #{id}
+    WHERE actors.id = $1
     ORDER BY movies.title;"
   @actor_movies = db_connection do |conn|
-    conn.exec(query)
+    conn.exec_params(query, [id])
   end
-erb :'actors/show'
+  erb :'actors/show'
 end
 
-get '/movies' do
-  # id = params[:id]
-  query = "SELECT movies.id, movies.title, movies.year, movies.rating, genres.name as genre, studios.name
-  FROM movies
-  JOIN genres
-  ON movies.genre_id = genres.id
-  JOIN studios
-  ON movies.studio_id = studios.id
-  ORDER BY movies.title
-  LIMIT 20;"
-  @movies = db_connection do |conn|
-    conn.exec(query)
+get '/movies/:id' do
+  id = params[:id]
+  query = "SELECT movies.title, movies.year, movies.synopsis, movies.rating, genres.name AS genre_name,
+  studios.name AS studio_name, actors.id, actors.name AS actor_name, cast_members.character
+    FROM movies
+    JOIN genres
+    ON movies.genre_id = genres.id
+    JOIN studios
+    ON movies.studio_id = studios.id
+    JOIN cast_members
+    ON cast_members.movie_id = movies.id
+    JOIN actors
+    ON cast_members.actor_id = actors.id
+    WHERE movies.id = $1;"
+  @movie_details = db_connection do |conn|
+    conn.exec(query, [id])
   end
-  erb :'movies/index'
+  erb :'movies/show'
 end
-# Visiting /movies will show a table of movies, sorted alphabetically by title.
-# The table includes the movie title, the year it was released, the rating, the genre,
-#  and the studio that produced it. Each movie title is a link to the details page for that movie.
 
 
-# get '/movies/:id' do
-# end
+# Visiting /movies/:id will show the details for the movie.
+# This page should contain information about the movie (including genre and studio)
+# as well as a list of all of the actors and their roles. Each actor name is a link to the
+# details page for that actor.
+
+# actors.name AS actor_name
+
+
+#{id}
